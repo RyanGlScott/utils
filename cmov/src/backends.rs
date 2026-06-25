ed97cc65b7894206657e9def92a6fdc6a09523ef
Ryan Scott <rscott@galois.com>
2026-06-25 15:48:31 -0400

    cmov: Always use software implementation

--- cmov/src/backends.rs
+++ cmov/src/backends.rs
@@ -1,17 +1,12 @@
 //! Backends for `cmov`, only one of which will be selected at compile-time.
 
 // Architecture-specific backends for target architectures with native predication instructions
-#[cfg(not(miri))]
-#[cfg(target_arch = "aarch64")]
+#[cfg(false)]
 mod aarch64;
-#[cfg(not(miri))]
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[cfg(false)]
 mod x86;
 
 // Fallback portable implementation for targets which don't have native predication instructions
 // (or if they do, aren't currently supported)
-#[cfg(any(
-    not(any(target_arch = "aarch64", target_arch = "x86", target_arch = "x86_64")),
-    miri
-))]
+#[cfg(true)]
 mod soft;
